4f3277f3c04079415137687e99953c1570c5a948
bintobaby <bintobaby07@gmail.com>
2018-03-15 14:17:07 +0530

    addd to git

--- HomeController.cs
+++ HomeController.cs
@@ -3,69 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SodetsoUsersManagement.Models;
-using SodetsoUsersManagement.Services;
-using System.Web.Security;
-namespace SodetsoUsersManagement.Controllers
+
+namespace MVCBarcounsil.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-        HomeService _HomeService = new HomeService();
-
-        [HttpGet]
         public ActionResult Index()
         {
-            if (Session["Email"] != null && Session["UserID"] != null)
-            {
-                ViewBag.ActiveMenu = "Dashboard";
-                return View(_HomeService.DashboardStats());
-            }
-            else
-            {
-                return RedirectToAction("Login");
-            }
-            
-        }
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-        public ActionResult Login()
-        {           
             return View();
         }
 
-        [HttpPost]
-        public ActionResult WebLogin(LoginModel model)
+        public ActionResult About()
         {
-            if (ModelState.IsValid)
-            {
-                var loginResult = _HomeService.UserAuthenticate(model);
+            ViewBag.Message = "Your app description page.";
 
-                if (loginResult.Email != null && loginResult.UserID != 0)
-                {                   
-                    Session["UserID"] = loginResult.UserID.ToString();
-                    Session["Email"] = loginResult.Email.ToString();
-                    Session["Firstname"] = loginResult.Firstname.ToString();
-                    Session["Position"] = loginResult.Position.ToString();
-
-                    model.Password = string.Empty;
-
-                    return RedirectToAction("Index");
-                }
-                else
-                {
-                    return RedirectToAction("Login");
-                }
-
-            }
-            return RedirectToAction("Login");
+            return View();
         }
 
-        public ActionResult SignOut()
+        public ActionResult Contact()
         {
-            FormsAuthentication.SignOut();
-            Session.Abandon(); // it will clear the session at the end of request
-            return RedirectToAction("Login");
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
 }
